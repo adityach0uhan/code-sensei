ece2f9bdf1e00f39a7aabc7adeab3800d0e3cccd
adityach0uhan <m4with6x@gmail.com>
2025-02-17 22:00:28 +0530

    created dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import CodeEditor from '@/components/CodeEditor';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
 
@@ -9,14 +9,20 @@ const page = async () => {
     if (!isUserAuthenticated) {
         return redirect('/login');
     }
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+    // const { getUser } = getKindeServerSession();
+    // const user = await getUser();
 
     return (
-        <div className='w-screen h-screen flex flex-col items-center justify-center'>
-            <p>Dashboard</p>
-            <p>Hello {user.given_name}</p>
-            <LogoutLink>Log out</LogoutLink>
+        <div className='w-screen  flex flex-wrap items-center justify-center'>
+            <div className=' w-full md:w-1/2  flex flex-col items-center '>
+                <div className='w-full h-max p-2 m-4 rounded-md '>
+                    <CodeEditor />
+                </div>
+            </div>
+
+            <div className='md:w-1/2 w-full min-h-96  h-full bg-yellow-600'>
+                ;lkfaslfka
+            </div>
         </div>
     );
 };
